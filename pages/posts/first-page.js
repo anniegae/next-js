a8fb0e94488c6542ad67b786d715f49de12fc301
김현욱 <gimhyeon-ug@gimhyeon-ug-ui-MacBookAir.local>
2024-04-09 20:13:51 +0900

    On branch main

---
+++ pages/posts/first-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FirstPost() {
+  return (
+    <div>
+      <h1>First Post</h1>
+      <img src="https://avatars.githubusercontent.com/u/105193807?v=4" alt="GitHub Avatar" />
+      <p>Some text</p>
+    </div>
+  );
+}
